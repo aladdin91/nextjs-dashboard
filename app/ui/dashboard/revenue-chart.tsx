b5b9ced26b6cbb36168d10db44bfeb22c2a38fda
Alaa Abdelbaki <30771660+aladdin91@users.noreply.github.com>
2024-01-18 16:25:11 +0100

    fishing Chapter 9 Streaming

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,13 +1,11 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+import { fetchRevenue } from '@/app/lib/data';
+
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue();
   const chartHeight = 350;
 
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
